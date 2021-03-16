493e64c3a12ed5dcd4ecdcd93f4742e456f5a800
MusicDev33 <smccowan33@gmail.com>
2021-03-16 10:44:28 -0600

    remove bodyparser, add routes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import mongoose from 'mongoose';
 
 import { Request, Response, NextFunction } from 'express';
 import express from 'express';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import passport from 'passport';
@@ -70,8 +69,8 @@ const cOpt: cors.CorsOptions = {
 }
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(accessControl);
---
+++ src/routes/expression/getters.ts
@@ -0,0 +1,42 @@
+import ExpressionService from '@services/expression.service';
+import { Request, Response } from 'express';
+import { IExpression } from '@models/expression.model';
+
+type ExpressionReq = {
+  pathogenProteins: string[],
+  pathogen: string,
+  interactionCategory: string,
+  interactionType: string,
+  genes: string[]
+}
+
+// this needs a request body, so this is a POST request
+export const getExpressionsByParamsRoute = async (req: Request, res: Response) => {
+
+  // Shallow clone, there are no complex data types used so this is okay
+  const body: ExpressionReq = JSON.parse(JSON.stringify(req.body));
+
+  const expressions: IExpression[] = [];
+
+  for (let gene of body.genes) {
+    for (let patProtein of body.pathogenProteins) {
+      const query = {
+        pathogen: body.pathogen,
+        pathogenProtein: patProtein,
+        interactionCategory: body.interactionCategory,
+        interactionType: body.interactionType,
+        gene: gene
+      }
+      const results = await ExpressionService.findModelsByQuery(query);
+
+      expressions.concat(results as IExpression[]);
+    }
+  }
+
+  if (expressions.length) {
+    return res.status(200).json({success: true, payload: expressions});
+  }
+
+  return res.status(500).json({success: false, msg: 'Something went wrong.'});
+
+}
