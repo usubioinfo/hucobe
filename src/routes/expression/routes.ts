500ec57ab8c0c5fad1a31c4fccf1c74cf0f00907
MusicDev33 <smccowan33@gmail.com>
2021-04-10 13:49:00 -0600

    Remove unused routes

--- src/routes/expression/routes.ts
+++ src/routes/expression/routes.ts
@@ -5,7 +5,5 @@ import * as RouteFunctions from './export';
 router.post('/new', RouteFunctions.getExpressionsByParamsRoute);
 router.get('/test/:test', RouteFunctions.testRoute);
 router.get('/annotations', RouteFunctions.getTissueAnnotationsRoute);
-router.get('/newexpid', RouteFunctions.getExpResultIdRoute);
-router.get('/result/:id', RouteFunctions.getExpResultRoute);
 
 export default router;
