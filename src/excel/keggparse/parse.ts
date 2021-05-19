ab9e1d2c9b61370b795ddc53a3dd72045d208a78
MusicDev33 <smccowan33@gmail.com>
2021-05-18 22:14:49 -0600

    fix

--- src/excel/keggparse/parse.ts
+++ src/excel/keggparse/parse.ts
@@ -78,9 +78,11 @@ export const readExcelKegg = async (fileName: string, sheet: number) => {
     }
     let genes = [];
 
-    if (obj['geneId']) {
+    if (typeof obj['geneId'] !== 'number') {
       console.log(obj['geneId']);
       genes = obj['geneId'].split('/');
+    } else {
+      genes = [String(obj['geneId'])];
     }
 
     for (let gene of genes) {
