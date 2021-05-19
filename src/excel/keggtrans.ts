2acdf8dd48b64308e6ebe810f38e0f70d2e634b5
MusicDev33 <smccowan33@gmail.com>
2021-05-18 18:21:17 -0600

    fix

--- src/excel/keggtrans.ts
+++ src/excel/keggtrans.ts
@@ -1,5 +1,24 @@
 require('tsconfig-paths/register');
 import { translateGenes } from './keggparse/parse';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+require('dotenv-defaults/config');
+
+const db = `mongodb://localhost:27017/${process.env.DB_NAME}`;
+
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Database Connected: ${db}`);
+});
+
+mongoose.connection.on('error', (err: any) => {
+  console.log('Database Error: ' + err);
+});
 
 (async () => {
   await translateGenes();
